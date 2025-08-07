9fc85405823e1ca45fe9330850259cdc89dd742a
celiarr <cramireznavarrete@mail.sfsu.edu>
2025-08-07 13:28:18 -0700

    test deploy

--- dogApp/app.js
+++ dogApp/app.js
@@ -28,7 +28,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', signUpOneRouter);
+app.use('/', mapRouter); ////first page that will be shown when deploying
 app.use('/users', usersRouter);
 app.use('/signUp-1', signUpOneRouter);
 app.use('/signUp-2', signUpTwoRouter);
